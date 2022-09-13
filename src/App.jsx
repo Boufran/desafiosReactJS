4c70952eb20daa79376c1990081110b970809964
Bouefe <fbou@live.com.ar>
2022-09-13 19:22:44 -0300

    desafioClase7

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <ItemListContainer texto="Desafio 6" />
+      <ItemListContainer texto="Desafio 7" />
+      <ItemDetailContainer />
     </>
   );
 }
