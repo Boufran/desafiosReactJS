65f09cc83facca04b3d00a1994ae358d25d5f0be
Bouefe <fbou@live.com.ar>
2022-09-13 10:57:09 -0300

    desafio6

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ItemListContainer from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ItemListContainer texto="Desafio 6" />
+    </>
+  );
+}
+
+export default App;
