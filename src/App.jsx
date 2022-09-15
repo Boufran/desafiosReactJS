2e2b479fedc36caf6e1013fb90e3cfca230cdf3d
Bouefe <fbou@live.com.ar>
2022-09-15 11:34:25 -0300

    desafioPrimerEntrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer/index";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import Cart from "./components/Cart";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <ItemListContainer texto="Desafio 7" />
-      <ItemDetailContainer />
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/categoria/:categoriaId" element={<ItemListContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/detalle/:detalleId" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
