b171dfd36efcefb9d33e193678390b81d47a4434
Bouefe <fbou@live.com.ar>
2022-09-20 11:37:39 -0300

    desafio9y10

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,34 +3,34 @@ import CartWidget from "../CartWidget";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-	return (
-		<div className="container">
-			<nav className="nav">
-				<div className="nav__brand">
-					<NavLink className="nav__link" to="/">
-						MiMarca
-					</NavLink>
-				</div>
-				<ul className="nav__list">
-					<li>
-						<NavLink className="nav__link" to="/categoria/films">
-							Películas
-						</NavLink>
-					</li>
-					<li>
-						<NavLink className="nav__link" to="/categoria/series">
-							Series
-						</NavLink>
-					</li>
-					<li>
-						<NavLink className="nav__link" to="cart">
-							<CartWidget />
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+  return (
+    <div className="container">
+      <nav className="nav">
+        <div className="nav__brand">
+          <NavLink className="nav__link" to="/">
+            Cuevano not dead :D
+          </NavLink>
+        </div>
+        <ul className="nav__list">
+          <li>
+            <NavLink className="nav__link" to="/categoria/films">
+              Películas
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav__link" to="/categoria/series">
+              Series
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav__link" to="cart">
+              <CartWidget />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
