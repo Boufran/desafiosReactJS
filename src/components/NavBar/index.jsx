d23f17343e368b7cde9f4d6f731c60a28691c419
Bouefe <fbou@live.com.ar>
2022-09-25 14:40:35 -0300

    desafioFirebase

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <nav className="nav">
         <div className="nav__brand">
           <NavLink className="nav__link" to="/">
-            Cuevan Not Dead :D
+            Cuevana Not Dead :D!
           </NavLink>
         </div>
         <ul className="nav__list">
